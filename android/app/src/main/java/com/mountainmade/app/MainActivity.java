195463ad7a94e64cd3b77ff3e3e0e4224adedd74
Mountain Made <alucardmlbb686@gmail.com>
2026-03-03 10:01:16 +0630

    Fix Android back button natively in MainActivity — goBack() before super

--- android/app/src/main/java/com/mountainmade/app/MainActivity.java
+++ android/app/src/main/java/com/mountainmade/app/MainActivity.java
@@ -1,5 +1,17 @@
 package com.mountainmade.app;
 
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    public void onBackPressed() {
+        WebView webView = getBridge().getWebView();
+        if (webView != null && webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
